9dedefc29bcb32212e7596545c5629c642c39fe6
Cedric Vallieu <cvallieu94@gmail.com>
2022-01-17 20:12:41 -0800

    testing new code

--- server.js
+++ server.js
@@ -18,11 +18,17 @@ app.get('/', (request, response) => {
 const getWeather = require('./modules/weather.js');
 // console.log(getWeather);
 //set endpoint pass the object containing method from module
-app.get('/weather', getWeather.getWeather);
+app.get('/weather', getWeather.getWeather());
 
 const getMovies = require('./modules/movies.js');
 // console.log(getMovies);
-app.get('/movies', getMovies.getMovies);
+app.get('/movies', getMovies.getMovies());
+
+//pull in the module
+const getYelp = require('./modules/yelp.js');
+//set endpoint pass the object containing method from module
+console.log(getYelp);
+app.get('/yelp', getYelp.getYelp);
 
 
 app.listen(PORT, () => console.log(`listing on port ${PORT}`));
